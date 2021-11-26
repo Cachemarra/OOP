9c1d4e971d04071079b5e2aa4083ab46e2028fa4
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2021-11-26 12:34:01 -0700

    Laboratory_User_Data_Later/PoolCursor class was added and finished.

---
+++ python/Laboratory_User_Data_Layer/PoolCursor.py
@@ -0,0 +1,68 @@
+#%% Pool Cursor class
+
+from os import close
+from psycopg2 import pool
+import logging as log
+import sys
+from Connection import Connection
+
+# Basic configuration for the logger
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p'
+                )
+
+
+#%% Creation of the class
+
+class PoolCursor:
+    '''
+    Manage the connections and cursor objects obtained by pool connection.
+    '''
+    def __init__(self):
+        self._conn = None
+        self._cursor = None
+
+    def __enter__(self):
+        # Method when using 'with'
+        # Getting the connection an the cursor.
+        self._conn = Connection.getConnection()
+        self._cursor = self._conn.cursor()
+
+        return self._cursor
+        
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        # Method called when exiting 'with'
+        # A commit or rollback will happened depending if there's an error
+        
+        # If exception_value exists, will apply a rollback
+        if exception_value:
+            self._conn.rollback()
+            log.error(f'[ERROR] An exception has ocurred, rollbacking. \n Error Message: {value}, {type}, {traceback}')
+        
+        # No errors nor exceptions
+        else:
+            # Commiting changes
+            self._conn.commit()
+            log.debug('[INFO] Transaction commited succesfully.')
+
+        # Closing the cursor
+        self._cursor.close()
+        Connection.freeConnection(self._conn)
+        
+
+#%% Testing PoolCursor class.
+if __name__ == '__main__':
+
+    # Remember, using 'with' the class is initialized automatically.
+
+    with PoolCursor() as cursor: # Will enter to __enter__ and it returns a cursor.
+        # Creating a query to the database.
+        cursor.execute('SELECT * FROM person')
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+    
+    sys.exit()
